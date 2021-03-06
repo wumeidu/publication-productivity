44b25021e3359fa37a952ea645f0f8b86b45001c
wumeidu <80076836+wumeidu@users.noreply.github.com>
2021-03-06 18:43:10 +0800

    Update model_trend_2000_another_test.m

--- model_trend_2000_another_test.m
+++ model_trend_2000_another_test.m
@@ -2,7 +2,7 @@ clear
 tagg=1
 predict_thresd=15
 
-fn =['D:\dududu\dblp_data\coauthor_evolu_data\dblp-rels-retag-1995-2019_1951_hyper_degree_squence_start_at_2000.txt'];
+fn =['D:\dududu\dblp_data\coauthor_evolu_data\dblp-rels-retag-1988-2019_1951_hyper_degree_squence_start_at_2000.txt'];
 fid =importdata(fn); 
    
 hyperdegrees_null_1= fid;
@@ -16,20 +16,20 @@ predict_hyperdegrees=sum(fid(:,1:13),2   );
 nodes_set_hyperdegree=predict_hyperdegrees;
 
 
-end_y=[2008:1:2019];
+end_y=[2001:1:2019];
 beta=-.00000
 gamma=20
 col=6
 row=3
 
 
-s2='model-trend-another-2000'
+s2='model-trend-2000'
 
-%读入hazard矩阵 
+
 set(gcf,'unit','normalized','position',[0.1,0.1,0.37 ,0.37])
-for ss=2008:2019%7:15%
-    fn=['D:\dududu\dblp_data\predict\LSTM_another\dblp-rels-retag-test-predict-', num2str(ss),'_curr.txt'];
-    fn_1=['D:\dududu\dblp_data\predict\LSTM_another\dblp-rels-retag-test-ture-', num2str(ss),'_curr.txt'];
+for ss=2001:2018%7:15%
+    fn=['D:\dblp-rels-retag-test-predict-', num2str(ss),'_curr_history_0.txt'];
+    fn_1=['D:\dblp-rels-retag-test-ture-', num2str(ss),'_curr_history_0.txt'];
   
     
     fid =importdata(fn);
@@ -67,15 +67,15 @@ for ss=2008:2019%7:15%
     hold on
     
     hold on
-    %subplot(1, length(end_y), ss)
+    
     plot(intial_hyperdegree_value,  predict_average_hyperdegree , '-b','LineWidth', 0.9 ,'markersize',0.8,'Color',[51/255,102/255,255/255] )
     hold on
     plot(intial_hyperdegree_value,    average_hyperdegree, 'o','LineWidth',0.001,'markersize',1.8 , 'MarkerFaceColor',[0.85,0.33,0.01], 'MarkeredgeColor',[0.85,0.33,0.01])
     hold on
    
     [rho,pval]= corr(predict_hyperdegrees ,nodes_set_hyperdegree,'Type', 'Pearson');
-    %plot(intial_hyperdegree_value,    intial_hyperdegree_value,   'g')
-    ss2=ss-2007;%;
+    
+    ss2=ss-2007;
     s3=['y=',num2str(end_y(ss2)) ];
     text((mod(tagg-1,col))/col+2,( 1-(ceil(1/col))/row+30-11 )*45/30   , s3 ,'FontSize', 8, 'Fontname', 'Times New Roman' )
     s3=['s_1=',num2str(round(rho,3))];
